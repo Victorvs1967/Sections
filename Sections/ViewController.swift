ffd32159791a0a7d36e0f7a098ac94dbcb559a7a
Victorvs1967 <victorsmirnov@me.com>
2017-12-03 15:17:11 +0200

    Create App Sections

--- Sections/ViewController.swift
+++ Sections/ViewController.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+  
+  var sectionTableIdentifier = "SectionTableIdentifier"
+  var names: [String: [String]]!
+  var keys: [String]!
 
+  @IBOutlet weak var tableView: UITableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    tableView.register(UITableViewCell.self, forCellReuseIdentifier: sectionTableIdentifier)
+    let path = Bundle.main.path(forResource: "sortednames", ofType: "plist")
+    let nameDict = NSDictionary(contentsOfFile: path!)
+    names = nameDict as! [String: [String]]
+    keys = (nameDict!.allKeys as! [String]).sorted()
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +31,28 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  // MARK: Table Data Source
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return keys.count
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    let key = keys[section]
+    let nameSection = names[key]!
+    return nameSection.count
+  }
+  
+  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return keys[section]
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: sectionTableIdentifier, for: indexPath)
+    let key = keys[indexPath.section]
+    let nameSection = names[key]!
+    cell.textLabel?.text = nameSection[indexPath.row]
+    return cell
+  }
+  
 }
 
